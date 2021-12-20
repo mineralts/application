0d06d442303c3a51372361f1ed97d76ebd43c665
Freeze455 <baptiste.parmantier.pro@gmail.com>
2021-12-20 16:27:25 +0100

    :construction: Work in progress

--- src/types/index.ts
+++ src/types/index.ts
@@ -7,3 +7,36 @@
  * file that was distributed with this source code.
  *
  */
+
+export type ApplicationNode = {
+  major: number
+  minor: number
+  patch: number
+  prerelease: (string | number)[]
+  version: string
+  toString(): string
+}
+
+export interface DirectoriesNode {
+  config: string
+  public: string
+  contracts: string
+  providers: string
+  start: string
+  tests: string
+  [key: string]: string
+}
+
+export type RcFile = {
+  preloads: any[]
+  commands: string[]
+  providers: string[]
+  statics: string[]
+  aliases: {
+    [key: string]: string
+  }
+}
+
+export interface ApplicationContract {
+  container: any
+}
